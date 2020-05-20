54a4789442ac78e682d069e8442261bdd64f37f8
rpdecks <rpdecks@gmail.com>
2020-05-19 22:17:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+
+let createEmployeeRecord = function (ary){
+    return {
+        firstName:      ary[0],
+        familyName:     ary[1],
+        title:          ary[2],
+        payPerHour:     ary[3],
+        timeInEvents:   [],
+        timeOutEvents:  []
+    }
+}
+
+let createEmployeeRecords = function(ary) {
+    return ary.map(createEmployeeRecord)
+}
+
+let createTimeInEvent = function() {
+
+}
+
+let createTimeOutEvent = function() {
+
+}
