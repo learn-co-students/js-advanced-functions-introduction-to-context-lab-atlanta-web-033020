8d904bfe93b0daceabf30e4138dabb27adcf32ab
rpdecks <rpdecks@gmail.com>
2020-05-20 16:20:45 -0400

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,63 @@ let createEmployeeRecords = function(ary) {
     return ary.map(createEmployeeRecord)
 }
 
-let createTimeInEvent = function() {
-
+let createTimeInEvent = function(record, dateTime) {
+    let [date, time] = dateTime.split(' ')
+    let timeInEvent = {}
+    timeInEvent = {
+        type:   'TimeIn',
+        hour:   parseInt(time),
+        date:   date
+    }
+    record.timeInEvents.push(timeInEvent)
+    return record
 }
 
-let createTimeOutEvent = function() {
+let createTimeOutEvent = function(record, dateTime) {
+    let [date, time] = dateTime.split(' ')
+    let timeOutEvent = {}
+    timeOutEvent = {
+        type:   'TimeOut',
+        hour:   parseInt(time),
+        date:   date
+    }
+    record.timeOutEvents.push(timeOutEvent)
+    return record
+}
 
+let hoursWorkedOnDate = function(record, date) {
+    let timeInEvent = record.timeInEvents.find(function(event) {
+        return event.date === date
+    })
+    let timeOutEvent = record.timeOutEvents.find(function(event) {
+        return event.date === date
+    })
+    let hoursWorked = Math.abs(timeOutEvent.hour - timeInEvent.hour)/100
+    return hoursWorked
+}
+
+let wagesEarnedOnDate = function(record, date) {
+    let payRate = record.payPerHour
+    let hoursWorked = hoursWorkedOnDate(record, date)
+    let payOwed = payRate * hoursWorked
+    return payOwed
+}
+
+let allWagesFor = function(record) {
+    let datesWorked = record.timeInEvents.map(event => event.date)
+    return datesWorked.reduce((payOwed, date) => {
+        return payOwed += wagesEarnedOnDate(record, date)
+    }, 0)
+}
+
+let findEmployeeByFirstName = function(records, fname) {
+    return records.find(function(employee){
+        return fname === employee.firstName
+    })
+}
+
+let calculatePayroll = function(records) {
+    return records.reduce((payroll, employee) => {
+        return payroll += allWagesFor(employee)
+    }, 0)
 }
